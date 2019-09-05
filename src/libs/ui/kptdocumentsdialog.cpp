550d3ff9f74131fef019ffeddb97e1145bf8477f
Dag Andersen <danders@get2net.dk>
2019-09-05 09:28:33 +0200

    DocumentsDialog: Add nodename to dialog

--- src/libs/ui/kptdocumentsdialog.cpp
+++ src/libs/ui/kptdocumentsdialog.cpp
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
- * Copyright (C) 2019 Dag Andersen <calligra-devel@kde.org>
+ * Copyright (C) 2019 Dag Andersen <danders@get2net.dk>
+ * Copyright (C) 2007, 2012 Dag Andersen <danders@get2net.dk>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -32,6 +33,7 @@ DocumentsDialog::DocumentsDialog(Node &node, QWidget *p, bool readOnly)
     : KoDialog(p)
     , m_panel(new DocumentsPanel(node, this))
 {
+    m_panel->addNodeName();
     setCaption(i18n("Task Documents"));
     if (readOnly) {
         setButtons(Close);
--- src/libs/ui/kptdocumentspanel.cpp
+++ src/libs/ui/kptdocumentspanel.cpp
@@ -1,21 +1,22 @@
 /* This file is part of the KDE project
-   Copyright (C) 2007, 2012 Dag Andersen <danders@get2net.dk>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Copyright (C) 2019 Dag Andersen <danders@get2net.dk>
+ * Copyright (C) 2007, 2012 Dag Andersen <danders@get2net.dk>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
-*/
+ */
 
 // clazy:excludeall=qstring-arg
 #include "kptdocumentspanel.h"
@@ -44,6 +45,7 @@ DocumentsPanel::DocumentsPanel( Node &node, QWidget *parent )
     m_docs( node.documents() )
 {
     widget.setupUi( this );
+    widget.nodeNameHolder->hide();
     QVBoxLayout *l = new QVBoxLayout( widget.itemViewHolder );
     m_view = new DocumentTreeView( widget.itemViewHolder );
     l->setMargin(0);
@@ -70,6 +72,12 @@ DocumentsPanel::DocumentsPanel( Node &node, QWidget *parent )
     if (model()->rowCount() > 0) {
         m_view->setCurrentIndex(model()->index(0, 0));
     }
+}
+
+void DocumentsPanel::addNodeName()
+{
+    widget.nodeName->setText(m_node.name());
+    widget.nodeNameHolder->show();
 }
 
 DocumentItemModel *DocumentsPanel::model() const
