78aa787807efbd1917a949e019a2f924c2e17cad
Dag Andersen <dag.andersen@kdemail.net>
2021-01-08 13:09:57 +0100

    Gantt printing: Fix Bug 430452 - Printing with row labels on multiple pages does not work well
    (cherry picked from commit b5b5141f47f226b9a8f0db065cbb2d191ded9fc2)

--- src/libs/ui/kptganttview.h
+++ src/libs/ui/kptganttview.h
@@ -32,6 +32,7 @@
 #include <KGanttGlobal>
 #include <KGanttView>
 #include <KGanttDateTimeGrid>
+#include <KGanttPrintingContext>
 
 class KoDocument;
 
@@ -62,7 +63,201 @@ class GanttPrintingOptions;
 class NodeGanttViewBase;
 class GanttPrintingOptionsWidget;
 class DateTimeTimeLine;
+<<<<<<< HEAD
 class GanttItemDelegate;
+=======
+
+//---------------------------------------
+class GanttChartDisplayOptionsPanel : public QWidget, public Ui::GanttChartDisplayOptions
+{
+    Q_OBJECT
+public:
+    explicit GanttChartDisplayOptionsPanel(GanttViewBase *gantt, GanttItemDelegate *delegate, QWidget *parent = 0);
+
+    void setValues(const GanttItemDelegate &del);
+
+public Q_SLOTS:
+    void slotOk();
+    void setDefault();
+
+Q_SIGNALS:
+    void changed();
+
+private:
+    GanttItemDelegate *m_delegate;
+    GanttViewBase *m_gantt;
+};
+
+class GanttViewSettingsDialog : public ItemViewSettupDialog
+{
+    Q_OBJECT
+public:
+    explicit GanttViewSettingsDialog(GanttViewBase *gantt, GanttItemDelegate *delegate, ViewBase *view, bool selectPrint = false);
+
+protected Q_SLOTS:
+    void slotOk() override;
+
+private:
+    GanttViewBase *m_gantt;
+    GanttPrintingOptionsWidget *m_printingoptions;
+};
+
+//--------------------
+class GanttPrintingOptions
+{
+public:
+    GanttPrintingOptions();
+    GanttPrintingOptions(const GanttPrintingOptions &other);
+
+    bool loadContext(const KoXmlElement &settings);
+    void saveContext(QDomElement &settings) const;
+
+    KGantt::PrintingContext context;
+    bool useStartTime;
+    QDateTime diagramStart;
+    bool useEndTime;
+    QDateTime diagramEnd;
+};
+
+class PLANUI_EXPORT GanttPrintingOptionsWidget : public QWidget, public Ui::GanttPrintingOptionsWidget
+{
+    Q_OBJECT
+public:
+    explicit GanttPrintingOptionsWidget(QWidget *parent = 0);
+
+    GanttPrintingOptions options() const;
+
+    void setPrintRowLabels(bool value) { ui_printRowLabels->setChecked(value); }
+    bool printRowLabels() const { return ui_printRowLabels->isChecked(); }
+
+    void setPrintColumnLabels(bool value) { ui_printColumnLabels->setChecked(value); }
+    bool printColumnLabels() const { return ui_printColumnLabels->isChecked(); }
+
+    void setFitting(const KGantt::PrintingContext::Fitting &fitting)  {
+        if (fitting & KGantt::PrintingContext::FitSinglePage) {
+            ui_fitSingle->setChecked(true);
+        } else if (fitting & KGantt::PrintingContext::FitPageHeight) {
+            ui_fitVertical->setChecked(true);
+        } else {
+            ui_noFitting->setChecked(true);
+        }
+    }
+    KGantt::PrintingContext::Fitting fitting() const {
+        KGantt::PrintingContext::Fitting s;
+        if (ui_fitSingle->isChecked()) {
+            s = KGantt::PrintingContext::FitSinglePage;
+        } else if (ui_fitVertical->isChecked()) {
+            s = KGantt::PrintingContext::FitPageHeight;
+        } else {
+            s = KGantt::PrintingContext::NoFitting;
+        }
+        return s;
+    }
+
+public Q_SLOTS:
+    void setOptions(const KPlato::GanttPrintingOptions &opt);
+};
+
+class GanttPrintingDialog : public PrintingDialog
+{
+    Q_OBJECT
+public:
+    GanttPrintingDialog(ViewBase *view, GanttViewBase *gantt);
+
+    void startPrinting(RemovePolicy removePolicy) override;
+    QList<QWidget*> createOptionWidgets() const override;
+    void printPage(int page, QPainter &painter) override;
+
+    int documentLastPage() const override;
+    qreal rowLabelsWidth(const QPaintDevice *device) const;
+    QRectF calcSceneRect(const QDateTime &startDateTime, const QDateTime &endDateTime) const;
+
+protected Q_SLOTS:
+    void slotPrintRowLabelsToogled(bool on);
+    void slotSinglePageToogled(bool on);
+
+protected:
+    GanttViewBase *m_gantt;
+    GanttPrintingOptionsWidget *m_options;
+    QRectF m_sceneRect;
+    int m_horPages;
+    int m_vertPages;
+    double m_headerHeight;
+    qreal m_rowLabelsWidth;
+    QImage m_labelsImage;
+    QImage m_image;
+};
+
+class PLANUI_EXPORT GanttTreeView : public TreeViewBase
+{
+    Q_OBJECT
+public:
+    explicit GanttTreeView(QWidget *parent);
+
+};
+
+class GanttZoomWidget : public QSlider {
+    Q_OBJECT
+public:
+    explicit GanttZoomWidget(QWidget *parent);
+
+    void setGrid(KGantt::DateTimeGrid *grid);
+
+    void setEnableHideOnLeave(bool hide);
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private Q_SLOTS:
+    void sliderValueChanged(int value);
+
+private:
+    bool m_hide;
+    KGantt::DateTimeGrid *m_grid;
+};
+
+class PLANUI_EXPORT GanttViewBase : public KGantt::View
+{
+    Q_OBJECT
+public:
+    explicit GanttViewBase(QWidget *parent);
+    ~GanttViewBase() override;
+
+    GanttTreeView *treeView() const;
+    GanttPrintingOptions printingOptions() const { return m_printOptions; }
+
+    virtual bool loadContext(const KoXmlElement &settings);
+    virtual void saveContext(QDomElement &settings) const;
+
+    DateTimeTimeLine *timeLine() const;
+
+    void editCopy();
+
+    void handleContextMenuEvent(const QModelIndex &idx, const QPoint &pos);
+
+public Q_SLOTS:
+    void setPrintingOptions(const KPlato::GanttPrintingOptions &opt);
+
+Q_SIGNALS:
+    void contextMenuRequested(const QModelIndex &idx, const QPoint &pos);
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    friend class GanttPrintingDialog;
+    GanttPrintingOptions m_printOptions;
+
+private:
+    QPoint m_dragStartPosition;
+    Qt::MouseButton m_mouseButton;
+    GanttZoomWidget *m_zoomwidget;
+};
+>>>>>>> b5b5141f... Gantt printing: Fix Bug 430452 - Printing with row labels on multiple pages does not work well
 
 class NodeGanttViewBase : public GanttViewBase
 {
