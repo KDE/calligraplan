738259f3f56b4982acacc6b0c3b9c3a50e434e21
Mickael Sergent <miko53@free.fr>
2025-05-18 22:33:15 +0200

    remove about deprecation of function type()

--- src/libs/models/kptdocumentmodel.cpp
+++ src/libs/models/kptdocumentmodel.cpp
@@ -389,7 +389,7 @@ QVariant DocumentItemModel::data(const QModelIndex &index, int role) const
         result = m_model.data(doc, index.column(), role);
     }
     if (result.isValid()) {
-        if (role == Qt::DisplayRole && result.type() == QVariant::String && result.toString().isEmpty()) {
+        if (role == Qt::DisplayRole && result.typeId() == QMetaType::QString && result.toString().isEmpty()) {
             // HACK to show focus in empty cells
             result = ' ';
         }
